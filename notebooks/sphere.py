0a8afb45ea4ea7481f62e5f9521b402e2cc87a2f
peekxc <matt.piekenbrock@gmail.com>
2021-10-06 18:18:27 -0400

    CI changes to valid yaml

--- notebooks/sphere.py
+++ notebooks/sphere.py
@@ -34,6 +34,30 @@ for angle in range(0, 360, 30):
 	ax.view_init(30, angle)
 	plt.pause(0.50)
 
+
+# %% 
+from src.tallem import TALLEM
+from src.tallem.cover import LandmarkCover
+
+from src.tallem.dimred import geodesic_dist, neighborhood_graph
+G = neighborhood_graph(p_ext, k = 15)
+D = geodesic_dist(G.A)
+
+cover = LandmarkCover(D, 25, scale = 1.5)
+pc = TALLEM(cover, local_map="iso2", n_components=3).fit_transform(X=p_ext,B=p_ext)
+
+
+import numpy as np
+mu = p_ext.mean(axis=0)
+dist_to_center = np.array([np.linalg.norm(p - mu) for p in p_ext])
+
+import matplotlib.pyplot as plt 
+for angle in range(0, 360, 30):
+	ax = plt.figure(figsize=(8,8)).add_subplot(projection='3d')
+	ax.scatter3D(*pc.T, c=dist_to_center)
+	ax.view_init(30, angle)
+	plt.pause(0.50)
+
 # from src.tallem.color import linear_gradient, bin_color
 # col_pal = linear_gradient(["red", "purple", "blue"], 25)['hex']
 
