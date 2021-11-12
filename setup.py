f08e0524642ef55be0dd52dc0d1a7529c36c82b1
peekxc <matt.piekenbrock@gmail.com>
2021-11-12 15:37:19 -0500

    Readding setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['tallem', 'tallem.extensions']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.21.3,<2.0.0', 'scipy>=1.6']
+
+extras_require = \
+{'autograd': ['autograd'],
+ 'pymanopt': ['pymanopt>=0.2.5'],
+ 'scikit-learn': ['scikit-learn>=1.0']}
+
+setup_kwargs = {
+    'name': 'tallem',
+    'version': '0.2.0',
+    'description': 'Topological Assembly of Locally Euclidean Models',
+    'long_description': '# Topological Assembly of Local Euclidean Models \n\nThis repository implements TALLEM - a topologically inspired non-linear dimensionality reduction method.\n\nGiven some data set *X* and a map <img class=\'latex-inline math\' style="background: white; vertical-align:-0.105206pt;" src="https://render.githubusercontent.com/render/math?math=\\large f%20%3A%20X%20%5Cto%20B&mode=inline"> onto some topological space _B_ which captures the topology/nonlinearity of _X_, TALLEM constructs a map <img style="background: white; vertical-align:-0.105206pt" class=\'latex-inline math\' src="https://render.githubusercontent.com/render/math?math=\\large F%20%3A%20X%20%5Cto%20%5Cmathbb%7BR%7D%5ED%20&mode=inline"> mapping _X_ to a _D_-dimensional space. \n\nTODO: describe TALLEM more\n\n## Installing + Dependencies \n\n`tallem`\'s run-time dependencies are fairly minimal. They include:  \n\n1. _Python >= 3.8.0_ \n2. *NumPy (>= 1.20)* and *SciPy* *(>=1.6)*\n\nThe details of the rest of package requirements are listed in [pyproject.toml](https://github.com/peekxc/tallem/blob/main/pyproject.toml). These are automatically downloaded and installed via `pip`: \n\n\n\nSome functions which extend TALLEM\'s core functionality require additional dependencies to be called---they include *autograd*, *pymanopt*, *scikit-learn*, and *bokeh*. These  packages are completely optional, i.e. they are not needed to get the resulting embedding. Nonetheless, if you would like these package as well, use: \n\n\n\n\n\n###Installing from cibuildwheels\n\nTODO\n\n### Installing from source\n\nTo install `tallem` from source, clone the repository and install the package via: \n\n```bash\npython -m pip install .\n```\n\n`tallem` relies on a few package dependencies in order to compile correctly when building from source. These libraries include: \n\n* [Armadillo](http://arma.sourceforge.net/) >= 10.5.2 ([see here for installation options](http://arma.sourceforge.net/download.html))\n* [Poetry](https://python-poetry.org/) (for building the [source](https://packaging.python.org/glossary/#term-Source-Distribution-or-sdist) and [binary](https://packaging.python.org/glossary/#term-Wheel) distributions)\n* [Meson](https://mesonbuild.com/) and [Ninja](https://ninja-build.org/) (for building the [extension modules](https://docs.python.org/3/glossary.html#term-extension-module))\n\nAn install attempt of these external dependencies is made if they are not available prior to call to `pip`, however these may require manual installation. Additionally, the current source files are written in [C++17](https://en.wikipedia.org/wiki/C%2B%2B17), so a [C++17 compliant compiler](https://en.cppreference.com/w/cpp/compiler_support/17) will be needed. If you have an installation problems or questions, feel free to [make a new issue](https://github.com/peekxc/tallem/issues).\n\n## Usage \n\nBelow is some example code showcasing TALLEMs ability to handle topological obstructions to dimensionality reduction like non-orientability.  \n\n```python\nfrom tallem import TALLEM\nfrom tallem.cover import IntervalCover\nfrom tallem.datasets import mobius_band\n\n## Get mobius band data + its parameter space\nX, B = mobius_band()\nB_polar = B[:,[1]]\n\n## Construct a cover over the polar coordinate\nm_dist = lambda x,y: np.sum(np.minimum(abs(x - y), (2*np.pi) - abs(x - y)))\ncover = IntervalCover(B_polar, n_sets = 10, overlap = 0.30, metric = m_dist)\n\n## Parameterize TALLEM + transform the data to the obtain the coordinization\nemb = TALLEM(cover=cover, local_map="cmds2", n_components=3).fit_transform(X, B_polar)\n\n## Draw the coordinates via 3D projection, colored by the polar coordinate\nimport matplotlib.pyplot as plt\nfig = plt.figure()\nax = fig.add_subplot(projection=\'3d\')\nax.scatter(*emb.T, marker=\'o\', c=B_polar)\n```\n\n![mobius band](https://github.com/peekxc/tallem/blob/main/resources/tallem_polar.png?raw=true)\n\n',
+    'author': 'Matt Piekenbrock',
+    'author_email': 'matt.piekenbrock@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/peekxc/tallem',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.8,<3.10',
+}
+from build import *
+build(setup_kwargs)
+
+setup(**setup_kwargs)
+
